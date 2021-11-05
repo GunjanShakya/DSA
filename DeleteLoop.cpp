50a6c42cbda4ca05260fbee3e17f3c72de7640ea
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2021-11-06 00:10:49 +0530

    Create DeleteLoop.cpp

---
+++ DeleteLoop.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+     bool hasCycle(Node *head) {
+        if(head==NULL || head->next==NULL)
+            return false;
+        Node * f=head,*s=head;
+        while(f!=NULL && f->next!= NULL )
+        {
+            f=f->next->next;
+            s=s->next;
+            if(s==f)
+                return true;
+        }
+        return false;
+    }
+    void removeLoop(Node* head)
+    { if(hasCycle(head)==false)
+    return;
+    Node *f=head,*s=head;
+   // f=f->next->next; s=s->next;
+    while(f->next && f->next->next)
+    {  f=f->next->next;
+        s=s->next;
+        if(f==s)
+        break;        
+    }    
+    if(f==head)
+    {
+        while(s->next!=head)
+        s=s->next;
+        s->next=NULL;
+        return;
+          } 
+     
+        f=head;
+        while(f->next!=s->next)
+        {
+            f=f->next; s=s->next;
+        } s->next=NULL;
+         }
+};
+
