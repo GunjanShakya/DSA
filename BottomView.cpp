e45caafd2b52dcfb809bbdd4171be6d30078853a
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2021-12-12 22:05:12 +0530

    Create BottomView.cpp

---
+++ BottomView.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+     map<int,int> m;
+    queue<pair<Node *,int>> q;
+    q.push({root,0}); vector<int> ans;
+    if(!root) return ans;
+    while(!q.empty())
+    {
+        Node *t=q.front().first;
+        int h=q.front().second;
+        q.pop();
+        if(t->left) q.push({t->left,h-1});
+        if(t->right) q.push({t->right,h+1});
+         m[h]=t->data;
+    } for(auto x:m) ans.push_back(x.second);
+    return ans;
+    }
+};
