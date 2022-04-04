c1f0007e7726320fe9e7be6e7262efb7b44d3c9f
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-04-04 22:39:15 +0530

    Time: 822 ms (62.24%), Space: 180.2 MB (15.97%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+         ListNode* low=head;
+        ListNode* high=head;
+        ListNode* curr=head;
+        int cnt=1;
+        while(curr!=NULL){
+            if(cnt<k){
+                low=low->next;
+            }
+            else if(cnt>k){
+                high=high->next;
+            }
+            curr=curr->next;
+            cnt++;
+        }
+        swap(low->val, high->val);
+        return head;
+    }
+};
