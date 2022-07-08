f223b270e57b100aba4a2497d597c5bd60650fa4
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-07-08 22:12:58 +0530

    Time: 3 ms (43.95%), Space: 5.9 MB (73.33%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -17,3 +17,24 @@ public:
         return ctr(m,n);
     }
 };
+
+/******************************************************************************************
+
+TLE - RECURSIVE SOLUTION
+
+int p(int sr,int sc,int dr,int dc)
+    {  
+        
+         if(sr==dr and sc==dc)
+            return 1;
+        if(sr>dr or sc>dc)
+            return 0;
+    else  return  p(sr+1,sc,dr,dc)
+      +  p(sr,sc+1,dr,dc);
+       // return ans;
+    }
+    int uniquePaths(int m, int n) {
+       int x=p(0,0,m-1,n-1);
+        return x;
+    }
+*********************************************************************************************/
