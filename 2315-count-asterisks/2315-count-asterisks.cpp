6914a342300df7fed61bf9f16d8ee9a390e5290b
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-07-09 15:04:16 +0530

    Time: 5 ms (29.89%), Space: 6.6 MB (5.37%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+         int c=0;
+        stack <char> st;
+        for (int i=0; i<s.length(); i++){
+            if (s[i]=='|'){
+                if (st.empty()){
+                    st.push(s[i]);
+                }
+                else{
+                    st.pop();
+                }
+            }
+            else if (s[i]=='*'){
+                if (st.empty()){
+                    c++;
+                }
+            }
+        }
+        return c;
+//         int ctr=0; vector<int> v;      
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(s[i]=='|')
+//             v.push_back(i);
+//         } int k=0;
+//         if(v.size()==0)
+//         {
+//             for(int i=0;i<s.size();i++)
+//                 if(s[i]=='*')
+//                     ctr++;
+//         } 
+//         else {
+//            for(int i=0;i<s.size();i++)
+//         { 
+//             if(i==v[k])
+//         {
+//             i=v[k+1]+1;
+//             k+=2;  
+//         } 
+//       else   if(s[i]=='*')
+//            ctr++;  
+//         } 
+//         }
+        
+//         return ctr;
+    }
+};
