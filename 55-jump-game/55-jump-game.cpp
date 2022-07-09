9f53138fa8c89fc3569abd551613b9a49379da5a
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-07-10 00:39:39 +0530

    Time: 127 ms (34.62%), Space: 48.2 MB (93.25%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int r=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(r<i)
+                return false;
+            r=max(r,i+nums[i]);
+        } return true;
+    }
+};
