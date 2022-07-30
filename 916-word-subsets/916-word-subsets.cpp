852ea716606731cd4bed5752906586599e825a10
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-07-31 01:40:24 +0530

    Time: 202 ms (87.00%), Space: 102.4 MB (40.31%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
+        vector<int>freq(26,0);
+        for(auto x:B){
+            vector<int>temp(26,0);
+            for(auto y:x){
+                temp[y-'a']++;
+                freq[y-'a'] = max(freq[y-'a'],temp[y-'a']); 
+            }
+        }
+        vector<string>res;
+        for(auto x:A){
+            vector<int>temp(26,0);
+            for(auto y:x) temp[y-'a']++;    
+            bool flag=true;
+            for(int i=0 ; i<26 ; i++)
+                if(freq[i]>temp[i]) {flag=false;break;}         
+            if(flag) res.push_back(x);
+        }
+        return res;
+    }
+};
