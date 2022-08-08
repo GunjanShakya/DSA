2eccf5a79e60fd44cce46bcaa31b9932a72722b9
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-08-08 15:04:39 +0530

    Time: 10 ms (85.36%), Space: 10.1 MB (29.24%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,27 +4,48 @@ public:
    int f(int n,int m,vector<vector<int>>& grid)
     {
         if(n<0 or m<0)
-            return 100000;
+            return 1e9;
         if(n==0 and m==0)
             return grid[0][0];
     
       int up=grid[n][m]+  f(n-1,m,grid);
         int left=grid[n][m]+f(n,m-1,grid);
         return min(up,left);
-    }*/
-     int f(int n,int m,vector<vector<int>>& grid, vector<vector<int>>& t)
+    }
+*********************************************************************************************
+              MEMOIZED SOLUTION -- ACCEPTED
+    
+    int f(int n,int m,vector<vector<int>>& grid, vector<vector<int>>& t)
      {
          if(n==0 and m==0) return grid[n][m];
-          if(t[n][m]!=-1) return t[n][m];
+          if(t[n][m]!=-1) return t[n][m];  //memoization
          int up=INT_MAX,left=INT_MAX;
        if(n>0)   up=grid[n][m]+f(n-1,m,grid,t);
         if(m>0)  left=grid[n][m]+f(n,m-1,grid,t);
          return t[n][m]=min(up,left);
              
-     }
+     } */
+    
+    
     int minPathSum(vector<vector<int>>& grid) {
-     int m=grid.size()-1,n=grid[0].size()-1;
-     vector<vector<int>>t(m+1,vector<int>(n+1,-1));
-        return f(m,n,grid,t);
+     int m=grid.size(),n=grid[0].size();
+     vector<vector<int>>t(m+1,vector<int>(n+1,0));
+        //return f(m,n,grid,t);
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 and j==0)
+                    t[i][j]=grid[i][j];
+                else
+                {
+                    int up=1e9,left=1e9;
+                    if(i>0 )up=grid[i][j]+t[i-1][j];
+                    if(j>0) left=grid[i][j]+t[i][j-1];
+                    t[i][j]=min(up,left);
+                }
+            }
+        } return t[m-1][n-1];
     }
 };
