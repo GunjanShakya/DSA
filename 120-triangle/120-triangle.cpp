067ebfb8a313ea23a81f1aef68ea702ce8228d69
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-08-08 17:23:47 +0530

    Time: 21 ms (5.92%), Space: 8.8 MB (44.93%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-   int dp[201][201];
-int check(vector<vector<int>>&arr, int i, int j){
-    if(i==arr.size())return 0;
-	
-    if(dp[i][j] != -1)return dp[i][j];
-	
-    int x=arr[i][j] + check(arr,i+1,j);
-    int z=arr[i][j] + check(arr,i+1,j+1);
-	
-    return dp[i][j] = min({x,z});
-}
-int minimumTotal(vector<vector<int>>& triangle) {
-    memset(dp,-1,sizeof(dp));
-    return check(triangle,0,0);
-}
+    int dp[201][201];
+    int f(vector<vector<int>> &t,int i, int j, int n)
+    {
+        if(i==n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+         int d=t[i][j]+f(t,i+1,j,n);
+        int dg=t[i][j]+f(t,i+1,j+1,n); 
+        return dp[i][j]=min(d,dg);
+        
+        
+    }
+    int minimumTotal(vector<vector<int>>& t) {
+    int n=t.size(),m=t[0].size();
+        memset(dp,-1,sizeof(dp));
+        return f(t,0,0,n);
+    }
 };
