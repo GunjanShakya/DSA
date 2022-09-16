cfeb2e66bc892c8d1bcc5cc31f5b73dfde15316d
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-09-16 16:35:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (81.49%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-   
     double pow(double x,int n)
-    {
+    { double ans;
         if(n==0)
             return 1;
-     double   ans=pow(x,n/2);
+       ans= pow(x,n/2);
         if(n%2==0)
             return ans*ans;
-        return x*ans*ans;
+     return x*ans*ans;
     }
     double myPow(double x, int n) {
-       if(n>0) return pow(x,n);
-        if(n<0) return pow(1/x,abs(n));
-        return 1;
+      double ans=0;
+        if(n>0)
+           return pow(x,n);
+        else if(n<0)
+         return pow(1/x,abs(n));
+ return 1;
     }
 };
