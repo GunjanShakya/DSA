dfca2b77e3abdb95f71fe8092239d5ea8197c14b
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-09-20 22:53:02 +0530

    Time: 62 ms (36.73%), Space: 30.8 MB (93.08%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,13 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// TC-->O((log n))^2;
 class Solution {
 public:
     int countNodes(TreeNode* root) {
        if(!root) return 0;
         int lh=hl(root);
         int rh=hr(root);
-        if(lh==rh) return (1<<lh)-1;
+        if(lh==rh) return (1<<lh)-1; //(1<<lh) means 2 to the power lh
         return 1+countNodes(root->left)+countNodes(root->right); 
     }
     int hl(TreeNode* root)
