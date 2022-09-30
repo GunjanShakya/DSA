06d0e6043234adaf8d7d3d91210dfa396f1296e5
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-09-30 14:50:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.71%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        long long x=a+b;
-        int y=x;
-        return y;
+       while(b)
+       {
+           // int temp=(a & b)<<1;
+           unsigned temp=(a & b); //left shift of negative value error
+           a=a^b;
+           b=temp<<1;
+       } return a;
     }
 };
