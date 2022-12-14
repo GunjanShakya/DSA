cfb90a302af2372a9dcde17b572e8fb0afb15caf
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2022-12-14 16:45:21 +0530

    Time: 5 ms (22.94%), Space: 7.7 MB (50.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       // int sum=0,sum1=0;
+       //  for(int i=0;i<nums.size();i+=2)
+       //      sum+=nums[i];
+       //  for(int i=1;i<nums.size();i+=2)
+       //      sum1+=nums[i];
+       //  return sum>sum1?sum:sum1;
+    int n=nums.size();
+        int res[n]; res[0]=nums[0];
+        // if(n==1)
+        //     return nums[0];
+        // res[0]=nums[0], res[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++)
+        //     res[i]=max(res[i-1],res[i-2]+nums[i]);
+        // return res[n-1];
+        for(int i=1;i<n;i++)
+        {  
+        int pick=nums[i]; if(i>1) pick+=res[i-2];
+          int  notpick=0+res[i-1];
+            res[i]=max(pick,notpick);
+        } return res[n-1];
+    }
+};
