5d7cdd7b62a71b1bc37f65ad634b04f0e4c250ed
GunjanShakya <93340830+GunjanShakya@users.noreply.github.com>
2024-02-04 23:44:31 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (9.41%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[mid]==0) 
+            {
+                swap(nums[low],nums[mid]);
+                low++; mid++;
+            }
+            else if(nums[mid]==1) mid++;
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+        /* Count number of 0s,1s and 2s TC-> O(2N)
+        int c0=0,c1=0,c2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0) c0++;
+            if(nums[i]==1) c1++;
+            if(nums[i]==2) c2++;
+        }
+        int k=0;
+        for(int i=0;i<c0;i++) nums[k++]=0;
+        for(int i=0;i<c1;i++) nums[k++]=1;
+        for(int i=0;i<c2;i++) nums[k++]=2;
+        */
+    }
+};
